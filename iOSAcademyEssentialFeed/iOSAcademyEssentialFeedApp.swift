156ee64a81b648aa68ca244bf209c9157f780748
Antonio210696 <Antonio210696>
2023-04-04 12:48:23 +0200

    Initial Commit

---
+++ iOSAcademyEssentialFeed/iOSAcademyEssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAcademyEssentialFeedApp.swift
+//  iOSAcademyEssentialFeed
+//
+//  Created by Antonio Epifani on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSAcademyEssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
