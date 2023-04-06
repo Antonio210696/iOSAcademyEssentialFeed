0f4772588cbbe08403a511d96020302a11150303
Antonio210696 <Antonio210696>
2023-04-06 13:37:56 +0200

    Create Feed Item Model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Antonio Epifani on 06/04/23.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
