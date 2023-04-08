b1e39def153756cd9e8180751726c360257e2e18
Antonio210696 <Antonio210696>
2023-04-08 18:57:19 +0200

    replace optional params with enums with success and failure cases

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+public enum HTTPClientResult {
+	case success(HTTPURLResponse)
+	case failure(Error)
+}
+
+public protocol HTTPClient {
+	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
+
 public final class RemoteFeedLoader {
 	private let url: URL
 	private let client: HTTPClient
@@ -23,17 +32,13 @@ public final class RemoteFeedLoader {
 	
 	public func load(completion: @escaping (Error) -> Void) {
 		// rfl is mapping an http error to a domain error
-		client.get(from: url) { error, response in
-			if response != nil {
+		client.get(from: url) { result in
+			switch result {
+			case .success:
 				completion(.invalidData)
-			} else {
+			case .failure:
 				completion(.connectivity)
 			}
 		}
 	}
-}
-
-public protocol HTTPClient {
-	func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
-	// we don t want the same error that we have in the RemoteFeedLoader
 }
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -76,18 +76,18 @@ class RemoteFeedLoaderTests: XCTestCase {
 		var error: Error?
 		var completions = [(Error) -> Void]()
 		
-		private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+		private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
 		
 		var requestedURLs: [URL] {
 			return messages.map { $0.url }
 		}
 		
-		func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+		func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
 			messages.append((url, completion))
 		}
 		
 		func complete(with error: Error, at index: Int = 0) {
-			messages[index].completion(error, nil)
+			messages[index].completion(.failure(error))
 		}
 		
 		func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -98,7 +98,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 				headerFields: nil
 			)
 			
-			messages[index].completion(nil, response)
+			messages[index].completion(.success(response!))
 		}
 	}
 }
