f2f432d4e9b5915caadb9d1d45652ca96add9f4c
Antonio210696 <Antonio210696>
2023-04-08 20:12:24 +0200

    use explicit json data for non 200 http response tests

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -39,8 +39,8 @@ public final class RemoteFeedLoader {
 		// rfl is mapping an http error to a domain error
 		client.get(from: url) { result in
 			switch result {
-			case let .success(data, _):
-				if let root = try? JSONDecoder().decode(Root.self, from: data){
+			case let .success(data, response):
+				if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data){
 					completion(.success(root.items))
 				} else {
 					completion(.failure(.invalidData))
