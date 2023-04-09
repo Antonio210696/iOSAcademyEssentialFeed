4da8f03b1f69e147be335a21889a12643d21339e
Antonio210696 <Antonio210696>
2023-04-09 19:42:35 +0200

    simplify type declaration with helper function

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -39,7 +39,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 	func test_load_deliversErrorOnClientError() {
 		let (sut, client) = makeSUT()
 		
-		expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity), when: {
+		expect(sut, toCompleteWith: failure(.connectivity), when: {
 			let clientError = NSError(domain: "Test", code: 0)
 			client.complete(with: clientError)
 		})
@@ -51,7 +51,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 		let samples = [199, 201, 300, 400, 500]
 		
 		samples.enumerated().forEach { index, code in
-			expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+			expect(sut, toCompleteWith: failure(.invalidData), when: {
 				let json = makeItemJson([])
 				client.complete(withStatusCode: code, data: json, at: index)
 			})
@@ -61,7 +61,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 	func test_load_deliversErrorOn200HTTPResponseWithInvalidJson()  {
 		let (sut, client) = makeSUT()
 		
-		expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+		expect(sut, toCompleteWith: failure(.invalidData), when: {
 			let invalidJson = Data("invalid json".utf8)
 			client.complete(withStatusCode: 200, data: invalidJson)
 		})
@@ -137,6 +137,10 @@ class RemoteFeedLoaderTests: XCTestCase {
 	private func makeItemJson(_ items: [[String: Any]]) -> Data {
 		let itemsJSON = ["items": items]
 		return try! JSONSerialization.data(withJSONObject: itemsJSON)
+	}
+	
+	private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+		return .failure(error)
 	}
 	
 	private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
