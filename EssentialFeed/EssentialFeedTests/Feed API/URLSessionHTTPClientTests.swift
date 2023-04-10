5f11617235cd8d2084c8630fabdac3f2597b72a1
Antonio210696 <Antonio210696>
2023-04-10 11:29:01 +0200

    Resumes data task on creation

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -15,7 +15,7 @@ class URLSessionHTTPClient {
 	}
 	
 	func get(from url: URL) {
-		session.dataTask(with: url) { _, _, _ in}
+		session.dataTask(with: url) { _, _, _ in}.resume()
 	}
 }
 class URLSessionHTTLClientTests: XCTestCase {
@@ -30,18 +30,44 @@ class URLSessionHTTLClientTests: XCTestCase {
 		XCTAssertEqual(session.receivedURLs, [url])
 	}
 	
+	func test_getFromURL_resumesDataTaskWithURL() {
+		let url = URL(string: "http://any-url.com")!
+		let session = URLSessionSpy()
+		let task = URLSessionDataTaskSpy()
+		session.stub(url: url, task: task)
+		let sut = URLSessionHTTPClient(session: session)
+		
+		sut.get(from: url)
+		
+		XCTAssertEqual(task.resumeCallCount, 1)
+	}
 	// MARK: - Helpers
 		
 	private class URLSessionSpy: URLSession {
 		var receivedURLs = [URL]()
+		private var stubs = [URL : URLSessionDataTask]()
+		
+		func stub(url: URL, task: URLSessionDataTask) {
+			stubs[url] = task
+		}
 		
 		override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
 			receivedURLs.append(url)
-			return FakeURLSessionDataTask()
+			return stubs[url] ?? FakeURLSessionDataTask()
 		}
 	}
 	
-	private class FakeURLSessionDataTask: URLSessionDataTask { }
+	private class FakeURLSessionDataTask: URLSessionDataTask {
+		override func resume() { }
+	}
+	private class URLSessionDataTaskSpy: URLSessionDataTask {
+		var resumeCallCount = 0
+		
+		override func resume() {
+			resumeCallCount += 1
+		}
+	}
 }
 
 // mocking subclasses of foundation classes can be dangerous because we dont have implementation detail about those classes and we do not override all their methods
+// we are coupling a lot the tests with the API implementation.
