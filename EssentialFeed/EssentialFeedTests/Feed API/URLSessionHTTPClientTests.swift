249f39be056ed28bf8b8fa98011fac526e0378b6
Antonio210696 <Antonio210696>
2023-04-10 11:54:49 +0200

    Refactor URLSession subclass mocking with protocol based mocking

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -8,10 +8,18 @@
 import XCTest
 import EssentialFeed
 
+protocol HTTPSession {
+	func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTask
+}
+
+protocol HTTPSessionTask {
+	func resume()
+}
+
 class URLSessionHTTPClient {
-	private let session: URLSession
+	private let session: HTTPSession
 	
-	init(session: URLSession) {
+	init(session: HTTPSession) {
 		self.session = session
 	}
 	
@@ -27,7 +35,7 @@ class URLSessionHTTLClientTests: XCTestCase {
 	
 	func test_getFromURL_resumesDataTaskWithURL() {
 		let url = URL(string: "http://any-url.com")!
-		let session = URLSessionSpy()
+		let session = HTTPSessionSpy()
 		let task = URLSessionDataTaskSpy()
 		session.stub(url: url, task: task)
 		let sut = URLSessionHTTPClient(session: session)
@@ -39,7 +47,7 @@ class URLSessionHTTLClientTests: XCTestCase {
 	
 	func test_getFromURL_failsOnRequestError() {
 		let url = URL(string: "http://any-url.com")!
-		let session = URLSessionSpy()
+		let session = HTTPSessionSpy()
 		let error = NSError(domain: "any-error", code: 1)
 		session.stub(url: url, error: error)
 		
@@ -61,19 +69,19 @@ class URLSessionHTTLClientTests: XCTestCase {
 	}
 	// MARK: - Helpers
 		
-	private class URLSessionSpy: URLSession {
+	private class HTTPSessionSpy: HTTPSession {
 		private var stubs = [URL : Stub]()
 		
 		private struct Stub {
-			let task: URLSessionDataTask
+			let task: HTTPSessionTask
 			let error: Error?
 		}
 		
-		func stub(url: URL, task: URLSessionDataTask = FakeURLSessionDataTask(), error: Error? = nil) {
+		func stub(url: URL, task: HTTPSessionTask = FakeURLSessionDataTask(), error: Error? = nil) {
 			stubs[url] = Stub(task: task, error: error)
 		}
 		
-		override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+		func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> HTTPSessionTask {
 			guard let stub = stubs[url] else {
 				fatalError("Couldnt find stub for \(url)")
 			}
@@ -83,13 +91,13 @@ class URLSessionHTTLClientTests: XCTestCase {
 		}
 	}
 	
-	private class FakeURLSessionDataTask: URLSessionDataTask {
-		override func resume() { }
+	private class FakeURLSessionDataTask: HTTPSessionTask {
+		func resume() { }
 	}
-	private class URLSessionDataTaskSpy: URLSessionDataTask {
+	private class URLSessionDataTaskSpy: HTTPSessionTask {
 		var resumeCallCount = 0
 		
-		override func resume() {
+		func resume() {
 			resumeCallCount += 1
 		}
 	}
@@ -97,3 +105,6 @@ class URLSessionHTTLClientTests: XCTestCase {
 
 // mocking subclasses of foundation classes can be dangerous because we dont have implementation detail about those classes and we do not override all their methods
 // we are coupling a lot the tests with the API implementation.
+
+// a way could be to use protocols with just the signature of the method that we previously overrode
+// We copy exactly the same methods that we have in the foundation classes, and we have added these methods only for testing purposes. 
