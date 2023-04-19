c77f4ddc8af4189009cb9b30c3e7acbeb3621bcf
Antonio210696 <Antonio210696>
2023-04-19 18:58:27 +0200

    Load command requests cache retrieval

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpy.swift
@@ -0,0 +1,54 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeedTests
+//
+//  Created by Antonio Epifani on 19/04/23.
+//
+
+import EssentialFeed
+
+class FeedStoreSpy: FeedStore {
+	typealias DeletionCompletion = (Error?) -> Void
+	typealias InsertionCompletion = (Error?) -> Void
+	
+	enum ReceivedMessage: Equatable {
+		case deleteCachedFeed
+		case insert([LocalFeedImage], Date)
+		case retrieve
+	}
+	
+	private(set) var receivedMessages = [ReceivedMessage]()
+	
+	private var deletionCompletions = [DeletionCompletion]()
+	private var insertionCompletions = [InsertionCompletion]()
+	
+	func retrieve() {
+		receivedMessages.append(.retrieve)
+	}
+	
+	func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+		deletionCompletions.append(completion)
+		receivedMessages.append(.deleteCachedFeed)
+	}
+	
+	func completeDeletion(with error: Error, at index: Int = 0) {
+		deletionCompletions[index](error)
+	}
+	
+	func completeDeletionSuccessfully(at index: Int = 0) {
+		deletionCompletions[index](nil)
+	}
+	
+	func completeInsertion(with error: Error, at index: Int = 0) {
+		insertionCompletions[index](error)
+	}
+	
+	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+		insertionCompletions.append(completion)
+		receivedMessages.append(.insert(feed, timestamp))
+	}
+	
+	func completeInsertionSuccessfully(at index: Int = 0) {
+		insertionCompletions[index](nil)
+	}
+}
