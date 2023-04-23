e92c153cd67d9b9b84c5ee8ffa1abd2033d538d3
Antonio210696 <Antonio210696>
2023-04-23 18:08:28 +0200

    Extract duplicate delete test code into a reusable helper method

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -170,15 +170,10 @@ class CodableFeedStoreTests: XCTestCase {
 	
 	func test_delete_hasNoSideEffectsOnEmptyCache() {
 		let sut = makeSUT()
-		let exp = expectation(description: "Wait for cache deletion")
 		
-		sut.deleteCachedFeed { deletionError in
-			
-			XCTAssertNil(deletionError, " Expected emptu cache deletion to succeed")
-			exp.fulfill()
-		}
+		let deletionError = deleteCache(from: sut)
 		
-		wait(for: [exp], timeout: 1.0)
+		XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
 		
 		expect(sut, toRetrieve: .empty)
 	}
@@ -187,14 +182,9 @@ class CodableFeedStoreTests: XCTestCase {
 		let sut = makeSUT()
 		insert((uniqueImageFeed().local, Date()), to: sut)
 		
-		let exp = expectation(description: "Wait for cache deletion")
+		let deletionError = deleteCache(from: sut)
 		
-		sut.deleteCachedFeed { deletionError in
-			XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
-			exp.fulfill()
-		}
-		
-		wait(for: [exp], timeout: 1.0)
+		XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
 		
 		expect(sut, toRetrieve: .empty)
 	}
@@ -206,6 +196,18 @@ class CodableFeedStoreTests: XCTestCase {
 		trackForMemoryLeaks(sut, file: file, line: line)
 		
 		return sut
+	}
+	
+	private func deleteCache(from sut: CodableFeedStore) -> Error? {
+		let exp = expectation(description: "Wait for cache deletion")
+		var deletionError: Error?
+		sut.deleteCachedFeed { receivedDeletionError in
+			deletionError = receivedDeletionError
+			exp.fulfill()
+		}
+		
+		wait(for: [exp], timeout: 1.0)
+		return deletionError
 	}
 	
 	private func expect(_ sut: CodableFeedStore, toRetrieve expectedResult: RetrievalCachedFeedResult, file: StaticString = #file, line: UInt = #line) {
