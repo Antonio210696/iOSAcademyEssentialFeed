ea7689b8117ec4af703c9d419c49f2ae4b80ce70
Antonio210696 <Antonio210696>
2023-04-24 20:01:52 +0200

    Clean up & undo all cache side-effects on `setUp` and `tearDown` to avoid sharing state between tests

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -9,6 +9,19 @@ import XCTest
 import EssentialFeed
 
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
+	
+	override func setUp() {
+		super.setUp()
+		
+		setupEmptyStoreState()
+	}
+	
+	override func tearDown() {
+		super.tearDown()
+		
+		undoStoreSideEffects()
+	}
+	
 	func test_load_deliversNoItemsOnEmptyCache() {
 		let sut = makeSUT()
 		
@@ -35,7 +48,7 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
 		let storeURL = testSpecificStoreURL()
 		let store = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
 		let sut = LocalFeedLoader(store: store, currentDate: Date.init)
-		  
+		
 		trackForMemoryLeaks(store, file: file, line: line)
 		trackForMemoryLeaks(sut, file: file, line: line)
 		
@@ -48,5 +61,17 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
 	
 	private func cachesDirectory() -> URL {
 		return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+	}
+	
+	private func deleteStoreArtifacts() {
+		try? FileManager.default.removeItem(at: testSpecificStoreURL())
+	}
+	
+	private func setupEmptyStoreState() {
+		deleteStoreArtifacts()
+	}
+	
+	private func undoStoreSideEffects() {
+		deleteStoreArtifacts()
 	}
 }
