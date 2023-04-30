b88b9a05f4d4d8a8de3411706d69c951e4b84b0e
Antonio210696 <Antonio210696>
2023-04-30 17:22:14 +0200

    Rendere loaded images from URL

--- EssentialFeed/EssentialFeediOS/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedViewController.swift
@@ -60,16 +60,18 @@ public final class FeedViewController: UITableViewController {
 		cell.locationContainer.isHidden = (cellModel.location == nil)
 		cell.locationLabel.text = cellModel.location
 		cell.descriptionLabel.text = cellModel.description
+		cell.feedImageView.image = nil
 		cell.feedImageContainer.startShimmering()
 		
-		tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak self] result in
-			cell.feedImageContainer.stopShimmering()
+		tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+			let data = try? result.get()
+			cell?.feedImageView.image = data.map(UIImage.init) ?? nil
+			cell?.feedImageContainer.stopShimmering()
 		}
 		return cell
 	}
 	
 	public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-		let cellModel = tableModel[indexPath.row]
 		tasks[indexPath]?.cancel()
 		tasks[indexPath] = nil
 	}
