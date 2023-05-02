af60432bf520c682154bf183f822fce5ab911e15
Antonio210696 <Antonio210696>
2023-05-02 11:11:20 +0200

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Antonio Epifani on 02/05/23.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+	let isLoading: Bool
+	var lastUpdated: String?
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Antonio Epifani on 02/05/23.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+	let feed: [FeedImage]
+}
