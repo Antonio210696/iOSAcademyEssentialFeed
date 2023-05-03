2e49998c16dbe5c4d5f0d0f28ec28bd61db75a28
Antonio210696 <Antonio210696>
2023-05-03 18:13:47 +0200

    Reuse table view cells from storyboard configuration

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Controllers/FeedViewControllerTests.swift
@@ -246,6 +246,17 @@ final class FeedViewControllerTests: XCTestCase {
 		XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected second cancelled image URL request once second image is not near visible")
 	}
 	
+	func test_feedImageView_doesNotRenderLoadedImageWhenNotVisibleAnymore() {
+		let (sut, loader) = makeSUT()
+		sut.loadViewIfNeeded()
+		loader.completeFeedLoading(with: [makeImage()])
+		
+		let view = sut.simulateFeedImageViewNotVisible(at: 0)
+		loader.completeImageLoading(with: anyImageData(), at: 0)
+		
+		XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
+	}
+	
 	// MARK: - Helpers
 	
 	private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -258,6 +269,10 @@ final class FeedViewControllerTests: XCTestCase {
 	
 	private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "http://any-url.com")!) -> FeedImage {
 		return FeedImage(id: UUID(), description: description, location: location, url: url)
+	}
+	
+	private func anyImageData() -> Data {
+		return UIImage.make(withColor: .red).pngData()!
 	}
 	
 	private func assertThat(_ sut: FeedViewController, isRendering feed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
@@ -348,12 +363,14 @@ private extension FeedViewController {
 		return feedImageView(at: index) as? FeedImageCell
 	}
 	
-	func simulateFeedImageViewNotVisible(at index: Int) {
+	@discardableResult
+	func simulateFeedImageViewNotVisible(at index: Int) -> FeedImageCell? {
 		let view = simulateFeedImageViewVisible(at: index)
 		
 		let delegate = tableView.delegate
 		let index = IndexPath(row: index, section: feedImageSection)
 		delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
+		return view
 	}
 	
 	func simulateFeedImageViewNearVisible(at row: Int) {
