fe330b30fb8a45b7dc2d8193d20b2ebc15ff38df
Antonio210696 <Antonio210696>
2023-05-12 20:45:56 +0200

    Moved view model types to separate files

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Antonio Epifani on 12/05/23.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+	public let isLoading: Bool
+	public var lastUpdated: String?
+}
