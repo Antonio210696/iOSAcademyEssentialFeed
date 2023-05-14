e23c5a6ab7e78a95f45c0ed3a5267167d7f3750e
Antonio210696 <Antonio210696>
2023-05-14 18:46:09 +0200

    image view shows loading image with no error on `FeedImagePresenter.didStartLoadingImage`

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImgePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImgePresenterTests.swift
@@ -1,10 +1,33 @@
 import XCTest
+import EssentialFeed
 
-final class FeedImagePresenter {
+final class FeedImagePresenter<View: FeedImageView, Image> where Image == View.Image {
 	
+	private let imageView: View
+	
+	init(imageView: View) {
+		self.imageView = imageView
+	}
+	
+	func didStartLoadingImageData(for: FeedImage) {
+		imageView.display(FeedImageViewModel(
+			isLoading: true,
+			shouldRetry: false,
+			image: nil))
+	}
 }
 
-protocol FeedImageView { }
+struct FeedImageViewModel<Image> {
+	let isLoading: Bool
+	let shouldRetry: Bool
+	let image: Image?
+}
+
+protocol FeedImageView {
+	associatedtype Image
+	
+	func display(_ model: FeedImageViewModel<Image>)
+}
 
 final class FeedImagePresenterTests: XCTestCase {
 	func test_init_doesNotSendMessagesToView() {
@@ -13,11 +36,19 @@ final class FeedImagePresenterTests: XCTestCase {
 		XCTAssertEqual(view.messages, [])
 	}
 	
+	func test_didStartLoadingImage_displaysLoadingImageAndNoRetryAction() {
+		let (sut, view) = makeSUT()
+		
+		sut.didStartLoadingImageData(for: uniqueImage())
+		
+		XCTAssertEqual(view.messages, [.display(isLoading: true, image: nil, error: false)])
+	}
+	
 	// MARK: - Helpers
 	
-	private func makeSUT() -> (sut: FeedImagePresenter, view: ViewSpy) {
-		let sut = FeedImagePresenter()
+	private func makeSUT() -> (sut: FeedImagePresenter<ViewSpy, FakeImage>, view: ViewSpy) {
 		let view = ViewSpy()
+		let sut = FeedImagePresenter(imageView: view)
 		
 		return (sut, view)
 	}
@@ -26,7 +57,16 @@ final class FeedImagePresenterTests: XCTestCase {
 		private(set) var messages: [Message] = []
 		
 		enum Message: Equatable {
-			
+			case display(isLoading: Bool, image: FakeImage?, error: Bool)
+		}
+		
+		func display(_ model: FeedImageViewModel<FakeImage>) {
+			messages.append(.display(
+				isLoading: model.isLoading,
+				image: model.image,
+				error: model.shouldRetry))
 		}
 	}
+	
+	private struct FakeImage: Equatable { }
 }
