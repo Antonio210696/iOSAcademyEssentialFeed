b1755b45072da8f675866d84b2aa2bfca159fee4
Antonio210696 <Antonio210696>
2023-05-14 19:53:37 +0200

    `FeedImagePresenter` methods preserve feed image model info, even with error, and forward them to image view

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImgePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImgePresenterTests.swift
@@ -13,11 +13,13 @@ final class FeedImagePresenter<View: FeedImageView, Image> where Image == View.I
 		self.imageDataTransformer = imageDataTransformer
 	}
 	
-	func didStartLoadingImageData(for: FeedImage) {
+	func didStartLoadingImageData(for model: FeedImage) {
 		imageView.display(FeedImageViewModel(
 			isLoading: true,
 			shouldRetry: false,
-			image: nil))
+			image: nil,
+			description: model.description,
+			location: model.location))
 	}
 	
 	func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
@@ -28,11 +30,18 @@ final class FeedImagePresenter<View: FeedImageView, Image> where Image == View.I
 		imageView.display(FeedImageViewModel(
 			isLoading: false,
 			shouldRetry: false,
-			image: image))
+			image: image,
+			description: model.description,
+			location: model.location))
 	}
 	
 	func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
-		imageView.display(FeedImageViewModel(isLoading: false, shouldRetry: true, image: nil))
+		imageView.display(FeedImageViewModel(
+			isLoading: false,
+			shouldRetry: true,
+			image: nil,
+			description: model.description,
+			location: model.location))
 	}
 }
 
@@ -40,6 +49,8 @@ struct FeedImageViewModel<Image> {
 	let isLoading: Bool
 	let shouldRetry: Bool
 	let image: Image?
+	let description: String?
+	let location: String?
 }
 
 protocol FeedImageView {
@@ -55,15 +66,21 @@ final class FeedImagePresenterTests: XCTestCase {
 		XCTAssertEqual(view.messages, [])
 	}
 	
-	func test_didStartLoadingImage_displaysLoadingImageAndNoRetryAction() {
+	func test_didStartLoadingImage_displaysLoadingImageAndNoRetryActionWithCorrectImageInfo() {
 		let (sut, view) = makeSUT()
 		
 		sut.didStartLoadingImageData(for: uniqueImage())
 		
-		XCTAssertEqual(view.messages, [.display(isLoading: true, image: nil, shouldRetry: false)])
+		XCTAssertEqual(view.messages, [.display(
+			isLoading: true,
+			image: nil,
+			shouldRetry: false,
+			description: uniqueImage().description,
+			location: uniqueImage().location)
+		])
 	}
 	
-	func test_didFinishLoadingImageData_showsRetryActionOnInvalidData() {
+	func test_didFinishLoadingImageData_showsRetryActionOnInvalidDataWithCorrectImageInfo() {
 		let (sut, view) = makeSUT()
 		
 		sut.didFinishLoadingImageData(with: anyInvalidData(), for: uniqueImage())
@@ -71,11 +88,13 @@ final class FeedImagePresenterTests: XCTestCase {
 		XCTAssertEqual(view.messages, [.display(
 			isLoading: false,
 			image: nil,
-			shouldRetry: true)
+			shouldRetry: true,
+			description: uniqueImage().description,
+			location: uniqueImage().location)
 		])
 	}
 	
-	func test_didFinishLoadingImageData_showsRetryActionOnError() {
+	func test_didFinishLoadingImageData_showsRetryActionOnErrorWithCorrectImageInfo() {
 		let (sut, view) = makeSUT()
 		
 		sut.didFinishLoadingImageData(with: anyNSError(), for: uniqueImage())
@@ -83,11 +102,13 @@ final class FeedImagePresenterTests: XCTestCase {
 		XCTAssertEqual(view.messages, [.display(
 			isLoading: false,
 			image: nil,
-			shouldRetry: true)
+			shouldRetry: true,
+			description: uniqueImage().description,
+			location: uniqueImage().location)
 		])
 	}
 	
-	func test_didFinishLoadingImageData_showsImageWithNoError() {
+	func test_didFinishLoadingImageData_showsImageWithNoErrorWithCorrectImageInfo() {
 		let (sut, view) = makeSUT()
 		
 		sut.didFinishLoadingImageData(with: anyData(), for: uniqueImage())
@@ -95,7 +116,9 @@ final class FeedImagePresenterTests: XCTestCase {
 		XCTAssertEqual(view.messages, [.display(
 			isLoading: false,
 			image: FakeImage(data: anyData()),
-			shouldRetry: false)
+			shouldRetry: false,
+			description: uniqueImage().description,
+			location: uniqueImage().location)
 		])
 	}
 	
@@ -124,14 +147,17 @@ final class FeedImagePresenterTests: XCTestCase {
 		private(set) var messages: [Message] = []
 		
 		enum Message: Equatable {
-			case display(isLoading: Bool, image: FakeImage?, shouldRetry: Bool)
+			case display(isLoading: Bool, image: FakeImage?, shouldRetry: Bool, description: String?, location: String?)
 		}
 		
 		func display(_ model: FeedImageViewModel<FakeImage>) {
 			messages.append(.display(
 				isLoading: model.isLoading,
 				image: model.image,
-				shouldRetry: model.shouldRetry))
+				shouldRetry: model.shouldRetry,
+				description: model.description,
+				location: model.location
+			))
 		}
 	}
 	
