50d3d91a8413ca2d27813403e02c62975e95b6b9
Antonio210696 <Antonio210696>
2023-05-21 12:52:56 +0200

    `FeedLoaderWithFallbackComposite.load` delivers fallback feed on primary loader failure

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -10,13 +10,22 @@ import EssentialFeed
 
 class FeedLoaderWithFallbackComposite: FeedLoader {
 	private let primary: FeedLoader
+	private let fallback: FeedLoader
 	
 	init(primary: FeedLoader, fallback: FeedLoader) {
 		self.primary = primary
+		self.fallback = fallback
 	}
 	
 	func load(completion: @escaping (Result<[EssentialFeed.FeedImage], Error>) -> Void) {
-		primary.load(completion: completion)
+		primary.load { [weak self] result in
+			switch result {
+			case .success:
+				completion(result)
+			case .failure:
+				self?.fallback.load(completion: completion)
+			}
+		}
 	}
 }
 
@@ -41,6 +50,27 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 		wait(for: [exp], timeout: 1.0)
 	}
 	
+	func test_load_deliversFallbackFeedOnPrimaryFailure() {
+		let fallbackFeed = uniqueFeed()
+		let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+		
+		let exp = expectation(description: "Wait for load completion")
+		
+		sut.load { result in
+			switch result {
+			case let .success(receivedFeed):
+				XCTAssertEqual(receivedFeed, fallbackFeed)
+				
+			case .failure:
+				XCTFail("Expected successful load feed result, got \(result) instead")
+			}
+			
+			exp.fulfill()
+		}
+		
+		wait(for: [exp], timeout: 1.0)
+	}
+	
 	private func makeSUT(primaryResult: FeedLoader.Result, fallbackResult: FeedLoader.Result, file: StaticString = #file, line: UInt = #line) -> FeedLoader {
 		let primaryLoader = LoaderStub(result: primaryResult)
 		let fallbackLoader = LoaderStub(result: fallbackResult)
@@ -59,6 +89,10 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 	
 	private func uniqueFeed() -> [FeedImage] {
 		return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
+	}
+	
+	private func anyNSError() -> NSError {
+		return NSError(domain: "any", code: 0)
 	}
 	
 	private class LoaderStub: FeedLoader {
