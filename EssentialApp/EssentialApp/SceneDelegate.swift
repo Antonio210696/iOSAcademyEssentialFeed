b09fa51d65c5da9d8b11a785fcc741a06bfd36da
Antonio210696 <Antonio210696>
2023-06-06 09:00:48 +0200

    call `configureWindow` method in `scene` method in `SceneDelegate`

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -36,6 +36,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let _ = (scene as? UIWindowScene) else { return }
 		
+		configureWindow()
 	}
 	
 	func configureWindow() {
