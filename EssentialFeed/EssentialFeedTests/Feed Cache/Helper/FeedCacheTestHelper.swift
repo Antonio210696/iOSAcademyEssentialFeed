bc84417b2bd8db6ab33b97474a67896e6b13cafe
Antonio210696 <Antonio210696>
2023-06-19 18:44:06 +0200

    Moved Date helpers in Shared scope

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helper/FeedCacheTestHelper.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helper/FeedCacheTestHelper.swift
@@ -27,17 +27,3 @@ extension Date {
 	private var feedCacheMaxAgeInDays: Int { 7 }
 	
 }
-
-extension Date {
-	func adding(seconds: TimeInterval) -> Date {
-		return self + seconds
-	}
-	
-	func adding(days: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
-		return calendar.date(byAdding: .day, value: days, to: self)!
-	}
-	
-	func adding(minutes: Int, calendar: Calendar = Calendar(identifier: .gregorian)) -> Date {
-		return calendar.date(byAdding: .minute, value: minutes, to: self)!
-	}
-}
