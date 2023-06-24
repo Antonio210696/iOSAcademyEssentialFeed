5a86c2fa9244ef146cdbd263e0ca51c27c5d158a
Antonio210696 <Antonio210696>
2023-06-24 12:04:48 +0200

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageCommentCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Antonio Epifani on 22/06/23.
+//
+
+import UIKit
+import EssentialFeed
+
+public class ImageCommentCellController: CellController {
+	private let model: ImageCommentViewModel
+	
+	public init(model: ImageCommentViewModel) {
+		self.model = model
+	}
+	
+	public func view(in tableView: UITableView) -> UITableViewCell {
+		let cell: ImageCommentCell = tableView.dequeuReusableCell()
+		cell.messageLabel.text = model.message
+		cell.usernameLabel.text = model.username
+		cell.dateLabel.text = model.date
+		
+		return cell
+	}
+	
+	public func preload() { }
+	
+	public func cancelLoad() { }
+}
---
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,40 @@
+//
+//  ImageCommentsSnapshotTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Antonio Epifani on 22/06/23.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+class ImageCommentsSnapshotTests: XCTestCase {
+	func test_listWithComments() {
+		let sut = makeSUT()
+		
+		sut.display(comments())
+		
+		assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "IMAGE_COMMENTS_light")
+		assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "IMAGE_COMMENTS_dark")
+	}
+	
+	
+	private func makeSUT() -> ListViewController {
+		let bundle = Bundle(for: ListViewController.self)
+		let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
+		let controller = storyboard.instantiateInitialViewController() as! ListViewController
+		controller.loadViewIfNeeded()
+		controller.tableView.showsVerticalScrollIndicator = false
+		controller.tableView.showsHorizontalScrollIndicator = false
+		return controller
+	}
+	
+	private func comments() -> [CellController] {
+		return [
+			ImageCommentCellController(model: ImageCommentViewModel(message: "Some description for this first image to be rendered going to write a very long text to see how the UI adapts to multiline text. This sometimes is painful but it is necessary", date: "1000 years ago", username: "a long long long username")),
+			ImageCommentCellController(model: ImageCommentViewModel(message: "Some description for this first image ", date: "10 days ago", username: "a username")),
+			ImageCommentCellController(model: ImageCommentViewModel(message: "nice", date: "1 hour ago", username: "a.")),
+		]
+	}
+}
