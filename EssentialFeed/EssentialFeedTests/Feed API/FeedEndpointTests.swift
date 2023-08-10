8212cdc1d2d40a3cf9fd742c15d3cdae81cc9131
Antonio210696 <Antonio210696>
2023-08-10 11:29:18 +0200

    Add feed endpoint with "after_id" param

--- EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -12,7 +12,7 @@ class FeedEndpointTests: XCTestCase {
 	func test_feed_endpointURL() {
 		let baseURL = URL(string: "http://base-url.com")!
 		
-		let received = FeedEndpoint.get.url(baseURL: baseURL)
+		let received = FeedEndpoint.get().url(baseURL: baseURL)
 		let expected = URL(string: "http://base-url.com/v1/feed?limit=10")!
 		
 		XCTAssertEqual(received, expected)
@@ -20,5 +20,18 @@ class FeedEndpointTests: XCTestCase {
 		XCTAssertEqual(received.host, "base-url.com", "host")
 		XCTAssertEqual(received.path, "/v1/feed", "path")
 		XCTAssertEqual(received.query, "limit=10", "query")
+	}
+	
+	func test_feed_endpointURLAfterGivenImage() {
+		let image = uniqueImage()
+		let baseURL = URL(string: "http://base-url.com")!
+		
+		let received = FeedEndpoint.get(after: image).url(baseURL: baseURL)
+		
+		XCTAssertEqual(received.scheme, "http", "scheme")
+		XCTAssertEqual(received.host, "base-url.com", "host")
+		XCTAssertEqual(received.path, "/v1/feed", "path")
+		XCTAssertTrue(received.query!.contains("limit=10"), "limit query param")
+		XCTAssertTrue(received.query!.contains("after_id=\(image.id)"), "after id param")
 	}
 }
