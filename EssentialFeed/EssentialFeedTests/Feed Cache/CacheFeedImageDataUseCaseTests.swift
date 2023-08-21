44b1b9bcfb5175d7c739bb0cbdafa5753fdc2f74
Antonio210696 <Antonio210696>
2023-08-21 12:31:36 +0200

    Save images synchronously

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedImageDataUseCaseTests.swift
@@ -43,20 +43,6 @@ class CacheFeedIMageDataUseCaseTests: XCTestCase {
 		})
 	}
 	
-	func test_saveImageDataFromURL_doesNOtDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-		let store = FeedImageDataStoreSpy()
-		var sut: LocalFeedImageDataLoader? = LocalFeedImageDataLoader(store: store)
-		
-		var received = [LocalFeedImageDataLoader.SaveResult]()
-		
-		sut?.save(anyData(), for: anyURL()) { received.append($0) }
-		
-		sut = nil
-		store.completeInsertionSuccessfully()
-		
-		XCTAssertTrue(received.isEmpty, "Expected no received result after instance has been deallocated")
-	}
-	
 	private func makeSUT(currentData: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: FeedImageDataStoreSpy) {
 		let store = FeedImageDataStoreSpy()
 		let sut = LocalFeedImageDataLoader(store: store)
@@ -72,6 +58,7 @@ class CacheFeedIMageDataUseCaseTests: XCTestCase {
 	
 	private func expect(_ sut: LocalFeedImageDataLoader, toCompleteWith expectedResult: LocalFeedImageDataLoader.SaveResult, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
 		let exp = expectation(description: "Wait for load completion")
+		action()
 		
 		sut.save(anyData(), for: anyURL()) { receivedResult in
 			switch (receivedResult, expectedResult) {
@@ -87,7 +74,6 @@ class CacheFeedIMageDataUseCaseTests: XCTestCase {
 			exp.fulfill()
 		}
 		
-		action()
 		wait(for: [exp], timeout: 1.0)
 	}
 }
