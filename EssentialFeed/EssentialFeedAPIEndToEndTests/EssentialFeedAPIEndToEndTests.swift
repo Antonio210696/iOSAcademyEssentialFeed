332cb9f7d94344185592cf738d99b5d25ee934da
Antonio210696 <Antonio210696>
2023-08-22 19:46:15 +0200

    Return Result<Data, Error> in API e2e tests, as we directly map data and response from the client in FeedImageDataMapper

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -47,19 +47,24 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
 	
 	// MARK: - Helpers
 	
-	private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
-		let loader = RemoteFeedImageDataLoader(client: ephemeralClient())
-		trackForMemoryLeaks(loader, file: file, line: line)
+	private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> Result<Data, Error>? {
+		let client = ephemeralClient()
 		
 		let exp = expectation(description: "Wait for load completion")
 		let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
 		
-		var receivedResult: FeedImageDataLoader.Result?
-		_ = loader.loadImageData(from: url) { result in
-			receivedResult = result
+		var receivedResult: Result<Data, Error>?
+		client.get(from: url) { result in
+			receivedResult = result.flatMap { (data, response) in
+				do {
+					return .success(try FeedImageDataMapper.map(data, response: response))
+				} catch {
+					return .failure(error)
+				}
+			}
 			exp.fulfill()
 		}
-		wait(for: [exp], timeout: 5.0)
+		wait(for: [exp])
 		
 		return receivedResult
 	}
