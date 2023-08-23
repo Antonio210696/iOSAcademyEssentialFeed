4871eefbae71b1e1ff3eb320e9c92501bbbfd45d
Antonio210696 <Antonio210696>
2023-08-23 12:46:08 +0200

    updated margins in SnapshotConfiguration

--- EssentialFeed/EssentialFeediOSTests/Helpers/SnapshotConfiguration.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/SnapshotConfiguration.swift
@@ -17,7 +17,7 @@ struct SnapshotConfiguration {
 		return SnapshotConfiguration(
 			size: CGSize(width: 390, height: 844),
 			safeAreaInserts: UIEdgeInsets(top: 47, left: 0, bottom: 34, right: 0),
-			layoutMargins: UIEdgeInsets(top: 47, left: 16, bottom: 34, right: 16),
+			layoutMargins: UIEdgeInsets(top: 55, left: 8, bottom: 42, right: 8),
 			traitCollection: UITraitCollection(traitsFrom: [
 				.init(forceTouchCapability: .available),
 				.init(layoutDirection: .leftToRight),
@@ -25,6 +25,7 @@ struct SnapshotConfiguration {
 				.init(userInterfaceIdiom: .phone),
 				.init(horizontalSizeClass: .compact),
 				.init(verticalSizeClass: .regular),
+				.init(accessibilityContrast: .normal),
 				.init(displayScale: 3),
 				.init(displayGamut: .P3),
 				.init(userInterfaceStyle: style)
